2f7bfa3c0f49c176442d30b2131902d659eb21f2
Mridulians <gmridul898@gmail.com>
2024-10-24 18:41:59 +0530

    new updates 4

--- back/config/db.js
+++ back/config/db.js
@@ -1,4 +1,19 @@
-import dotenv from "dotenv"; // Make sure to import dotenv
+// import dotenv from "dotenv"; // Make sure to import dotenv
+// import mongoose from "mongoose";
+
+// // Load environment variables
+// dotenv.config();
+
+// const mongoDbUrl = process.env.MONGODB_URL; 
+
+// console.log(typeof(mongoDbUrl))
+
+// export const connectDB = async () => {
+//   await mongoose.connect(mongoDbUrl).then(() => console.log("DB CONNECTED"));
+// };
+
+
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 
 // Load environment variables
@@ -6,6 +21,18 @@ dotenv.config();
 
 const mongoDbUrl = process.env.MONGODB_URL;
 
+// Log the MongoDB URL for debugging purposes
+console.log("MongoDB URL:", mongoDbUrl);
+
 export const connectDB = async () => {
-  await mongoose.connect(mongoDbUrl).then(() => console.log("DB CONNECTED"));
+  try {
+    await mongoose.connect(mongoDbUrl, {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+    });
+    console.log("DB CONNECTED");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1); // Exit the process with a failure
+  }
 };
