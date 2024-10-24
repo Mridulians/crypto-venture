432d4dbb8865b2db5f7abed95b166da657d89641
Mridulians <gmridul898@gmail.com>
2024-10-24 18:23:57 +0530

    new updates 3

--- back/config/db.js
+++ back/config/db.js
@@ -1,5 +1,8 @@
-// import dotenv from "dotenv";
+import dotenv from "dotenv"; // Make sure to import dotenv
 import mongoose from "mongoose";
+
+// Load environment variables
+dotenv.config();
 
 const mongoDbUrl = process.env.MONGODB_URL;
 
